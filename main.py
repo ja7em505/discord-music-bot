21c1645545c67137587eb87be91f531e9a136da9
ja7em505 <ja7em505@users.noreply.github.com>
2026-05-28 10:19:41 +0300

    Fix token to use env vars directly

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import asyncio
 import discord
 from discord.ext import commands
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-TOKEN = os.getenv("DISCORD_TOKEN")
-PREFIX = os.getenv("PREFIX", "!")
+TOKEN = os.environ.get("DISCORD_TOKEN") or os.getenv("DISCORD_TOKEN")
+PREFIX = os.environ.get("PREFIX") or os.getenv("PREFIX", "!")
 
 intents = discord.Intents.default()
 intents.message_content = True
